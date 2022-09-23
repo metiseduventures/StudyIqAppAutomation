1c4f0f0462d7f6a559ffc88441b0b5b3b72d0ef9
shubham bansal <shubham.bansal@adda247.com>
2022-09-23 11:29:00 +0530

    homepage test script

--- src/main/java/applicationUtil/HomePageUtil.java
+++ src/main/java/applicationUtil/HomePageUtil.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import pageObject.CoursePageDetailsVerify_OR;
 import pageObject.HomePage_OR;
 import pojo.testdata.TestData;
 import util.Common_Function;
@@ -18,11 +19,520 @@ public class HomePageUtil {
 	public Common_Function cfObj = new Common_Function();
 	public LoginUtil loginutillObj;
 	public List<String> homePageMsglist = new ArrayList<String>();
+	CoursePageDetailsVerify_OR cpdv_ORObj;
+	CourseDetailPage courseDetailPageUtil;
 
 	public HomePageUtil(AppiumDriver<MobileElement> driver) {
 
 		homePageORObj = new HomePage_OR();
 		PageFactory.initElements(new AppiumFieldDecorator(driver), homePageORObj);
+	}
+
+	public boolean verifyHomePage(AppiumDriver<MobileElement> driver, TestData testData) {
+		boolean result = true;
+		loginutillObj = new LoginUtil(driver);
+		try {
+			boolean checkLoginPageOrNot = loginutillObj.checkSignUpLoginPage(driver);
+			if (checkLoginPageOrNot == true) {
+
+				result = loginutillObj.doSignUp(driver);
+				if (!result) {
+					homePageMsglist.addAll(loginutillObj.loginMsgList);
+					return result;
+				}
+
+			} else {
+
+				result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.IMG_CLOSE, "id", 30);
+				if (result) {
+					cfObj.commonClick(cfObj.commonGetElement(driver, ConstantUtil.IMG_CLOSE, "id"));
+				}
+
+				result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.NAV_LIB, "id", 30);
+				if (!result) {
+					homePageMsglist.add("Home page not opened after login");
+					return result;
+				}
+			}
+
+			result = verifyHomeTabElements(driver);
+			if (!result) {
+				return result;
+			}
+
+			result = verifyImgSlider(driver);
+			if (!result) {
+				return result;
+			}
+
+			result = verifyCoursesOnHomePage(driver, testData);
+			if (!result) {
+				return result;
+			}
+
+			result = verifyHomePageBottomNavbar(driver);
+			if (!result) {
+				return result;
+			}
+
+			result = verifyHomePageTopToolbar(driver, testData);
+			if (!result) {
+				return result;
+			}
+
+		} catch (Exception e) {
+			homePageMsglist.add("verifyHomePage_Exception");
+			result = false;
+		}
+		return result;
+	}
+
+	public boolean verifyHomePageTopToolbar(AppiumDriver<MobileElement> driver, TestData testData) {
+		boolean result = true;
+		boolean bool = true;
+		try {
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.listOfTextView().get(0), 10);
+			if (!result) {
+				homePageMsglist.add("The home page title is not visible at top");
+				return result;
+			}
+
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.searchCourseBtn(), 10);
+			if (!result) {
+				homePageMsglist.add("The search course btn is not visible");
+				return result;
+			}
+
+			cfObj.commonClick(homePageORObj.searchCourseBtn());
+
+			cfObj.commonSetTextTextBox(homePageORObj.inputSearchBox(), "bank");
+
+			driver.hideKeyboard();
+
+			List<MobileElement> listOfTitles = homePageORObj.listOfSearchItemsTitle();
+
+			for (int i = 0; i < listOfTitles.size(); i++) {
+				result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.listOfSearchItemsTitle().get(i),
+						10);
+				if (!result) {
+					homePageMsglist.add("The search item title is not visible");
+					return result;
+				}
+			}
+			cfObj.scrollUtill(driver, 1);
+
+			cfObj.commonClick(homePageORObj.listOfSearchItemsImg().get(0));
+
+			result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.BUY_ONE, "id", 30);
+			if (!result) {
+				System.out.println("Course detail page not opened when click on course from search");
+				return result;
+			}
+
+			result = verifyBuyNowProcess(driver, testData);
+			if (!result) {
+				return result;
+			}
+
+			while (bool) {
+				driver.navigate().back();
+				result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.NAV_LIB, "id", 5);
+				if (result) {
+					bool = false;
+				} else {
+					bool = true;
+				}
+			}
+
+			result = verifyNavigationMenu(driver);
+			if (!result) {
+				return result;
+			}
+
+		} catch (Exception e) {
+			homePageMsglist.add("verifyHomePageToolbar_Exception");
+			result = false;
+		}
+		return result;
+	}
+
+	public boolean verifyNavigationMenu(AppiumDriver<MobileElement> driver) {
+		boolean result = true;
+		boolean bool = true;
+		try {
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.openNavigationMenu(), 10);
+			if (!result) {
+				homePageMsglist.add("The naviagtion menu btn not visible");
+				return result;
+			}
+			openNaviagationMenu();
+
+			List<MobileElement> navigationMenuElements = homePageORObj.getListNavigationMenuElements();
+
+			result = cfObj.commonWaitForElementToBeVisible(driver, navigationMenuElements.get(0), 10);
+			if (!result) {
+				homePageMsglist.add("The home btn is not visible");
+				return result;
+			}
+			cfObj.commonClick(navigationMenuElements.get(0));
+
+			openNaviagationMenu();
+
+			int n = navigationMenuElements.size();
+			for (int i = 1; i < n; i++) {
+				bool = true;
+				result = cfObj.commonWaitForElementToBeVisible(driver, navigationMenuElements.get(i), 10);
+				if (!result) {
+					homePageMsglist.add("The menu element is not visible");
+					return result;
+				}
+
+				String logOutText = navigationMenuElements.get(i).getText();
+				if (logOutText.equalsIgnoreCase("Log out")) {
+					cfObj.commonClick(navigationMenuElements.get(i));
+
+					cfObj.commonClick(homePageORObj.logoutBtn());
+
+					break;
+				}
+
+				cfObj.commonClick(navigationMenuElements.get(i));
+
+				while (bool) {
+					driver.navigate().back();
+					result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.NAV_LIB, "id", 5);
+					if (result) {
+						bool = false;
+					} else {
+						bool = true;
+					}
+				}
+
+				openNaviagationMenu();
+
+			}
+
+		} catch (Exception e) {
+			result = false;
+			homePageMsglist.add("verifyNavigationMenu_Exception: " + e.getMessage());
+		}
+		return result;
+	}
+
+	public boolean verifyHomeTabElements(AppiumDriver<MobileElement> driver) {
+		boolean result = true;
+		boolean cond = true;
+
+		try {
+
+			List<MobileElement> textOfTabElements = homePageORObj.listOfHomeTabText();
+			List<MobileElement> imgOfTabElements = homePageORObj.listOfHomeTabImgs();
+			int n = textOfTabElements.size();
+			for (int i = 0; i < n - 1; i++) {
+
+				result = cfObj.commonWaitForElementToBeVisible(driver, textOfTabElements.get(i), 10);
+				if (!result) {
+					homePageMsglist.add("The text in tab is not visible");
+					return result;
+				}
+
+				result = imgOfTabElements.get(i).isDisplayed();
+				if (!result) {
+					homePageMsglist.add("The img is not displayed with text");
+					return result;
+				}
+
+				cfObj.commonClick(textOfTabElements.get(i));
+
+			}
+
+			cfObj.swipeLeftOnElement(textOfTabElements.get(n - 1), driver);
+
+			while (cond) {
+				cfObj.swipeRightOnElement(homePageORObj.listOfHomeTabText().get(0), driver);
+
+				if (homePageORObj.listOfHomeTabText().get(0).getText().equalsIgnoreCase("Home")) {
+					cfObj.commonClick(homePageORObj.listOfHomeTabText().get(0));
+					cond = false;
+				}
+			}
+
+		} catch (Exception e) {
+			result = false;
+			homePageMsglist.add("verifyHomeTabElements_Exception: " + e.getMessage());
+		}
+
+		return result;
+	}
+
+	public boolean verifyImgSlider(AppiumDriver<MobileElement> driver) {
+		boolean result = true;
+		int count = 4;
+		try {
+			for (int k = 0; k < count; k++) {
+				result = homePageORObj.imgSlider().isDisplayed();
+				if (!result) {
+					homePageMsglist.add("The image is not visible");
+					return result;
+				}
+
+				cfObj.swipeLeftOnElement(homePageORObj.imgSlider(), driver);
+			}
+
+			cfObj.swipeRightOnElement(homePageORObj.imgSlider(), driver);
+			cfObj.commonClick(homePageORObj.imgSlider());
+
+			result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.BUY_ONE, "id", 30);
+			if (!result) {
+				System.out.println("Course detail page not opened when click on course from home page");
+				return result;
+			}
+
+			driver.navigate().back();
+
+		} catch (Exception e) {
+			result = false;
+			homePageMsglist.add("verifyImgSlider_Exception: " + e.getMessage());
+		}
+		return result;
+	}
+
+	public boolean verifyCoursesOnHomePage(AppiumDriver<MobileElement> driver, TestData testData) {
+		boolean result = true;
+		boolean bool = true;
+		courseDetailPageUtil = new CourseDetailPage(driver);
+		try {
+			List<MobileElement> titlesOfSectionCourses = homePageORObj.sectionOfCoursesTitles();
+
+			for (int i = 0; i < titlesOfSectionCourses.size(); i++) {
+
+				result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.sectionOfCoursesTitles().get(i),
+						10);
+				if (!result) {
+					homePageMsglist.add("The title of section course is not visible");
+					return result;
+				}
+
+				result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.viewAllBtnList().get(i), 10);
+				if (!result) {
+					homePageMsglist.add("The view all is not visible");
+					return result;
+				}
+			}
+
+			cfObj.commonClick(homePageORObj.getListCourses().get(0));
+
+			result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.BUY_ONE, "id", 30);
+			if (!result) {
+				System.out.println("Course detail page not opened when click on course from home page");
+				return result;
+			}
+
+			result = verifyBuyNowProcess(driver, testData);
+			if (!result) {
+				return result;
+			}
+
+			while (bool) {
+				driver.navigate().back();
+				result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.NAV_LIB, "id", 5);
+				if (result) {
+					bool = false;
+				} else {
+					bool = true;
+				}
+			}
+
+			cfObj.commonClick(homePageORObj.viewAllBtnList().get(0));
+
+			List<MobileElement> titleOfCoursesInViewAll = homePageORObj.listOftitleOfCoursesInViewAll();
+			List<MobileElement> imgOfCoursesInViewAll = homePageORObj.listOfImgOfCoursesInViewAll();
+
+			for (int i = 0; i < titleOfCoursesInViewAll.size(); i++) {
+
+				result = cfObj.commonWaitForElementToBeVisible(driver, titleOfCoursesInViewAll.get(i), 10);
+				if (!result) {
+					homePageMsglist.add("The title of the course is not visible");
+					return result;
+				}
+
+				result = imgOfCoursesInViewAll.get(i).isDisplayed();
+				if (!result) {
+					homePageMsglist.add("The img of the course is not visible");
+					return result;
+				}
+
+				cfObj.scrollUtill(driver, 1);
+			}
+
+			cfObj.commonClick(imgOfCoursesInViewAll.get(0));
+
+			driver.navigate().back();
+
+			cfObj.swipeLeftOnElement(homePageORObj.getListCourses().get(0), driver);
+
+			cfObj.scrollUtill(driver, 5);
+
+			cfObj.swipeLeftOnElement(homePageORObj.getListCourses().get(0), driver);
+			cfObj.swipeRightOnElement(homePageORObj.getListCourses().get(0), driver);
+
+		} catch (Exception e) {
+			result = false;
+			homePageMsglist.add("verifyCoursesOnHomePage_Exception: " + e.getMessage());
+		}
+		return result;
+	}
+
+	public boolean verifyHomePageBottomNavbar(AppiumDriver<MobileElement> driver) {
+		boolean result = true;
+		try {
+			// my library
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.getListBottomMenuMyLibrary().get(0),
+					10);
+			if (!result) {
+				homePageMsglist.add("The text of my library on bottom is not visible");
+				return result;
+			}
+			String libraryBottomText = homePageORObj.getListBottomMenuMyLibrary().get(0).getText();
+
+			cfObj.commonClick(homePageORObj.getListBottomMenuMyLibrary().get(0));
+
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.listOfTextView().get(0), 10);
+			if (!result) {
+				homePageMsglist.add("My library title is not visible at top");
+				return result;
+			}
+			String libraryPageText = homePageORObj.listOfTextView().get(0).getText();
+			if (libraryBottomText.equalsIgnoreCase(libraryPageText)) {
+				result = true;
+			} else {
+				homePageMsglist.add("The title and bottom text are not same");
+				return false;
+			}
+
+			// my testseries
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.getListBottomMenuMyTestSeries().get(0),
+					10);
+			if (!result) {
+				homePageMsglist.add("The text of my testseries on bottom is not visible");
+				return result;
+			}
+			String testseriesBottomText = homePageORObj.getListBottomMenuMyTestSeries().get(0).getText();
+
+			cfObj.commonClick(homePageORObj.getListBottomMenuMyTestSeries().get(0));
+
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.listOfTextView().get(0), 10);
+			if (!result) {
+				homePageMsglist.add("My testseries title is not visible at top");
+				return result;
+			}
+			String testSeriesPageText = homePageORObj.listOfTextView().get(0).getText();
+			if (testseriesBottomText.equalsIgnoreCase(testSeriesPageText)) {
+				result = true;
+			} else {
+				homePageMsglist.add("The title and bottom text are not same");
+				return false;
+			}
+
+			// my books
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.getListBottomMenuMyBook().get(0), 10);
+			if (!result) {
+				homePageMsglist.add("The text of my books on bottom is not visible");
+				return result;
+			}
+			String booksBottomText = homePageORObj.getListBottomMenuMyBook().get(0).getText();
+
+			cfObj.commonClick(homePageORObj.getListBottomMenuMyBook().get(0));
+
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.listOfTextView().get(0), 10);
+			if (!result) {
+				homePageMsglist.add("My books title is not visible at top");
+				return result;
+			}
+			String booksPageText = homePageORObj.listOfTextView().get(0).getText();
+			if (booksBottomText.equalsIgnoreCase(booksPageText)) {
+				result = true;
+			} else {
+				homePageMsglist.add("The title and bottom text are not same");
+				return false;
+			}
+
+			// my home
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.getListBottomMenuMyHome().get(0), 10);
+			if (!result) {
+				homePageMsglist.add("The text of my home on bottom is not visible");
+				return result;
+			}
+			String homeBottomText = homePageORObj.getListBottomMenuMyHome().get(0).getText();
+
+			cfObj.commonClick(homePageORObj.getListBottomMenuMyHome().get(0));
+
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.listOfTextView().get(0), 10);
+			if (!result) {
+				homePageMsglist.add("My home title is not visible at top");
+				return result;
+			}
+			String homePageText = homePageORObj.listOfTextView().get(0).getText();
+			if (homePageText.equalsIgnoreCase(homeBottomText)) {
+				result = true;
+			} else {
+				homePageMsglist.add("The title and bottom text are not same");
+				return false;
+			}
+
+			// explore
+			result = cfObj.commonWaitForElementToBeVisible(driver, homePageORObj.getListBottomMenuMyExplore().get(0),
+					10);
+			if (!result) {
+				homePageMsglist.add("The text of my home on bottom is not visible");
+				return result;
+			}
+
+			cfObj.commonClick(homePageORObj.getListBottomMenuMyExplore().get(0));
+
+			List<MobileElement> exploreSubjectTitles = homePageORObj.getListOfSubjectTitle();
+			List<MobileElement> exploreSubjectImgs = homePageORObj.getListOfSubjectImgs();
+
+			for (int i = 0; i < exploreSubjectTitles.size(); i++) {
+
+				result = cfObj.commonWaitForElementToBeVisible(driver, exploreSubjectTitles.get(i), 10);
+				if (!result) {
+					homePageMsglist.add("The title is not visible");
+					return result;
+				}
+
+				result = cfObj.commonWaitForElementToBeVisible(driver, exploreSubjectImgs.get(i), 10);
+				if (!result) {
+					homePageMsglist.add("The img is not visible");
+					return result;
+				}
+			}
+
+			cfObj.commonClick(exploreSubjectTitles.get(0));
+
+			List<MobileElement> exploreCourses = homePageORObj.exploreActionbarElements();
+
+			for (int i = 0; i < exploreCourses.size(); i++) {
+
+				result = cfObj.commonWaitForElementToBeVisible(driver, exploreCourses.get(i), 10);
+				if (!result) {
+					homePageMsglist.add("The courses on action bar is not visible");
+					return result;
+				}
+
+				cfObj.swipeLeftOnElement(exploreCourses.get(i), driver);
+
+				cfObj.commonClick(exploreCourses.get(i));
+			}
+
+			cfObj.commonClick(homePageORObj.getListBottomMenuMyHome().get(0));
+
+		} catch (Exception e) {
+			result = false;
+
+			homePageMsglist.add("verifyHomePageNavbar_Exception: " + e.getMessage());
+		}
+		return result;
 	}
 
 	public boolean clickOnCourseOnHomePage(AppiumDriver<MobileElement> driver) {
@@ -46,6 +556,7 @@ public class HomePageUtil {
 			result = cfObj.commonWaitForElementToBeLocatedAndVisible(driver, ConstantUtil.BUY_ONE, "id", 30);
 			if (!result) {
 				System.out.println("Course detail page not opened when click on course from home page");
+				return result;
 			}
 
 		} catch (Exception e) {
@@ -231,7 +742,7 @@ public class HomePageUtil {
 			cfObj.commonClick(homePageORObj.openNavigationMenu());
 
 		} catch (Exception e) {
-			System.out.println("error " + e.getMessage());
+			System.out.println("openNavigationMenuException " + e.getMessage());
 		}
 	}
 
@@ -252,6 +763,72 @@ public class HomePageUtil {
 
 		} catch (Exception e) {
 			System.out.println("error " + e.getMessage());
+			return result;
+		}
+		return result;
+	}
+
+	public boolean verifyBuyNowProcess(AppiumDriver<MobileElement> driver, TestData testData) {
+		boolean result = true;
+		try {
+
+			result = courseDetailPageUtil.clickOnBuyNow(driver);
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+			result = courseDetailPageUtil.verifyEMIoption(driver);
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+			result = courseDetailPageUtil.verifyPacks(driver);
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+			result = courseDetailPageUtil.selectCoupon_verifyAmount(driver);
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+			result = courseDetailPageUtil.changeCoupon(driver);
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+			result = courseDetailPageUtil.applyManualCoupon(driver);
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+			result = courseDetailPageUtil.chooseYourPack(driver, testData.getChoosePack());
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+			result = courseDetailPageUtil.buyNowPack(driver);
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+			result = courseDetailPageUtil.verifyViewDetails(driver);
+			if (!result) {
+				homePageMsglist.addAll(courseDetailPageUtil.coursePageMsgList);
+				return result;
+			}
+
+		} catch (Exception e) {
+			homePageMsglist.add("verifyBuyNowProcess_Exception " + e.getMessage());
+			return result;
 		}
 		return result;
 	}
--- src/main/java/pageObject/HomePage_OR.java
+++ src/main/java/pageObject/HomePage_OR.java
@@ -7,6 +7,49 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 
 public class HomePage_OR {
 
+	@AndroidFindBy(xpath = "//android.view.ViewGroup/android.widget.TextView")
+	private List<MobileElement> listOfTextViewElements;
+	
+	public List<MobileElement> listOfTextView() {
+		return listOfTextViewElements;
+	}
+	
+	@AndroidFindBy(id = "action_search")
+	private MobileElement searchCourseBtnElement;
+	
+	public MobileElement searchCourseBtn() {
+		return searchCourseBtnElement;
+	}
+	
+	@AndroidFindBy(id = "et_search")
+	private MobileElement inputSearchBoxElement;
+	
+	public MobileElement inputSearchBox() {
+		return inputSearchBoxElement;
+	}
+	
+	@AndroidFindBy(id = "item_search_title")
+	private List<MobileElement> listOfSearchItemTitleElements;
+	
+	public List<MobileElement> listOfSearchItemsTitle(){
+		return listOfSearchItemTitleElements;
+	}
+	
+	@AndroidFindBy(id = "item_search_img")
+	private List<MobileElement> listOfSearchItemImgElements;
+	
+	public List<MobileElement> listOfSearchItemsImg(){
+		return listOfSearchItemImgElements;
+	}
+	
+	
+	@AndroidFindBy(id = "bt_clear")
+	private MobileElement clearInputSearchElement;
+	
+	public MobileElement clearInputSearch() {
+		return clearInputSearchElement;
+	}
+		
 	@AndroidFindBy(id = "iv_home_section_landscape")
 	private List<MobileElement> listCourses;
 
@@ -45,6 +88,13 @@ public class HomePage_OR {
 
 	@AndroidFindBy(id = "item_search_img")
 	private List<MobileElement> listSearchResult;
+	
+	@AndroidFindBy(id = "tv_nav_home")
+	private List<MobileElement> listBottomMenuMyHome;
+
+	public List<MobileElement> getListBottomMenuMyHome() {
+		return listBottomMenuMyHome;
+	}
 
 	@AndroidFindBy(id = "tv_nav_library")
 	private List<MobileElement> listBottomMenuMyLibrary;
@@ -52,12 +102,26 @@ public class HomePage_OR {
 	public List<MobileElement> getListBottomMenuMyLibrary() {
 		return listBottomMenuMyLibrary;
 	}
+	
+	@AndroidFindBy(id = "tv_nav_explore")
+	private List<MobileElement> listBottomMenuMyExplore;
+
+	public List<MobileElement> getListBottomMenuMyExplore() {
+		return listBottomMenuMyExplore;
+	}
 
 	@AndroidFindBy(id = "tv_nav_test_series")
 	private List<MobileElement> listBottomMenuMyTestSeries;
 
 	public List<MobileElement> getListBottomMenuMyTestSeries() {
 		return listBottomMenuMyTestSeries;
+	}
+	
+	@AndroidFindBy(id = "tv_nav_book")
+	private List<MobileElement> listBottomMenuMyBook;
+
+	public List<MobileElement> getListBottomMenuMyBook() {
+		return listBottomMenuMyBook;
 	}
 
 	@AndroidFindBy(id = "iv_quick_login_close")
@@ -81,4 +145,87 @@ public class HomePage_OR {
 		return listNavigationMenuElements;
 	}
 
+	@AndroidFindBy(id = "img_slider")
+	private MobileElement imgSliderElement;
+	
+	public MobileElement imgSlider() {
+		return imgSliderElement;
+	}
+	
+	@AndroidFindBy(id = "txt_section_title")
+	private List<MobileElement> sectionOfCoursesTitle;
+	
+	public List<MobileElement> sectionOfCoursesTitles(){
+		return sectionOfCoursesTitle;
+	}
+	
+	@AndroidFindBy(id = "tv_home_view_all")
+	private List<MobileElement> viewAllBtnElements;
+	
+	public List<MobileElement> viewAllBtnList(){
+		return viewAllBtnElements;
+	}
+	
+	@AndroidFindBy(id = "tv_home_tab")
+	private List<MobileElement> listOfHomeTabTexts;
+	
+	public List<MobileElement> listOfHomeTabText() {
+		return listOfHomeTabTexts;
+	}
+	
+	@AndroidFindBy(id = "iv_home_tab")
+	private List<MobileElement> listOfHomeTabImgs;
+	
+	public List<MobileElement> listOfHomeTabImgs() {
+		return listOfHomeTabImgs;
+	}
+	
+	@AndroidFindBy(id = "ss_txt_title")
+	private List<MobileElement> listOfSubjectTitle;
+	
+	public List<MobileElement> getListOfSubjectTitle() {
+		return listOfSubjectTitle;
+	}
+	
+	@AndroidFindBy(id = "ss_img")
+	private List<MobileElement> listOfSubjectImgs;
+	
+	public List<MobileElement> getListOfSubjectImgs() {
+		return listOfSubjectImgs;
+	}
+	
+	@AndroidFindBy(xpath = "//androidx.appcompat.app.ActionBar.b")
+	private List<MobileElement> exploreActionBarElements;
+	
+	public List<MobileElement> exploreActionbarElements(){
+		return exploreActionBarElements;
+	}
+	
+	@AndroidFindBy(id = "//androidx.recyclerview.widget.RecyclerView")
+	private MobileElement viewOnExploreCourses;
+	
+	public MobileElement viewOnExploreCoursesPage() {
+		return viewOnExploreCourses;
+	}
+	
+	@AndroidFindBy(id = "android:id/button1")
+	private MobileElement logoutBtnElement;
+	
+	public MobileElement logoutBtn() {
+		return logoutBtnElement;
+	}
+	
+	@AndroidFindBy(id = "iv_view_all_thumb")
+	private List<MobileElement> imgOfCoursesInViewAll;
+	
+	public List<MobileElement> listOfImgOfCoursesInViewAll(){
+		return imgOfCoursesInViewAll;
+	}
+	
+	@AndroidFindBy(id = "tv_view_all_title")
+	private List<MobileElement> titleOfCoursesInViewAll;
+	
+	public List<MobileElement> listOftitleOfCoursesInViewAll(){
+		return titleOfCoursesInViewAll;
+	}
 }
